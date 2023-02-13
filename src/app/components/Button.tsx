9d43ea7470cf779291b107117ef7b313cba8abd7
João Alves <joao.alves@ecosis.com.br>
2023-02-13 11:06:01 -0300

    :heavy_plus_sign: Add clsx

--- src/app/components/Button.tsx
+++ src/app/components/Button.tsx
@@ -1,10 +1,16 @@
-interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {}
+import clsx from 'clsx'
 
-export function Button({ children, ...props }: ButtonProps) {
+export interface ButtonProps
+    extends React.ButtonHTMLAttributes<HTMLButtonElement> {}
+
+export function Button({ children, className, ...props }: ButtonProps) {
     return (
-        <button 
+        <button
             {...props}
-            className='h-12 rounded shadow text-white transition-colors bg-emerald-600 uppercase hover:bg-emerald-700 disabled:bg-zinc-300 disabled:text-zinc-500 disabled:cursor-not-allowed'
+            className={clsx(
+                'h-12 w-full rounded shadow text-white transition-colors bg-emerald-600 uppercase hover:bg-emerald-700 disabled:bg-zinc-300 disabled:text-zinc-500 disabled:cursor-not-allowed',
+                className
+            )}
         >
             {children}
         </button>
--- src/app/components/TextField.tsx
+++ src/app/components/TextField.tsx
@@ -1,10 +1,12 @@
+import clsx from 'clsx'
+
 interface TextFieldProps extends React.InputHTMLAttributes<HTMLInputElement> {}
 
-export function TextField(props: TextFieldProps) {
+export function TextField({className, ...props}: TextFieldProps) {
     return (
         <input 
             {...props}
-            className='p-3 rounded-sm bg-gray-200 focus:outline-none focus:ring focus:ring-emerald-500/80'
+            className={clsx('p-3 rounded-sm w-full bg-zinc-200 focus:outline-none focus:ring focus:ring-emerald-500/80', className)}
         />
     )
 }
